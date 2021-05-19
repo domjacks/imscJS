7102e401f9c4ffd8213fe195133feb0d7ef3f91c
Robert Bryer <robert.bryer@bbc.co.uk>
2021-05-19 10:47:18 +0100

    Whitespace change

--- src/main/js/html.js
+++ src/main/js/html.js
@@ -1445,14 +1445,14 @@ var backgroundColorAdjustSuffix = "BackgroundColorAdjust";
                     }
 
                     // prune later duplicates we may have inserted 
-                    if (rslt.length>0) {
-                        var unique=[rslt[0]];
-                        for (var fi=1;fi<rslt.length;fi++) {
+                    if (rslt.length > 0) {
+                        var unique = [rslt[0]];
+                        for (var fi = 1; fi < rslt.length; fi++) {
                             if (unique.indexOf(rslt[fi]) == -1) {
                                 unique.push(rslt[fi]);
                             }
                         }
-                        rslt=unique;
+                        rslt = unique;
                     }
 
                     dom_element.style.fontFamily = rslt.join(",");
